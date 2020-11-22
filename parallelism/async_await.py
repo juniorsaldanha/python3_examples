7b5a1f6e89ab7113c6cbd070d1d0a562b1f3cf2b
Junior Saldanha <umsaldanha@gmail.com>
2020-11-22 17:47:52 -0300

    async, await, asyncio.sleep, and take care about network connectio in async functions, as socket connection

---
+++ parallelism/async_await.py
@@ -0,0 +1,28 @@
+import time
+import asyncio
+
+def is_prime(num):
+    return not any(num//i == num/i for i in range(num-1, 1, -1))
+
+async def get_highest_prime(num):
+    print(f"Highest prime below {num}")
+    for i in range(num-1, 0, -1):
+        if is_prime(i):
+            print(f"⮞ Highest prime below {num} is {i}")
+            return i
+        await asyncio.sleep(0.01)
+    return None
+
+async def start():
+    time0 = time.time()
+    await asyncio.wait([
+        get_highest_prime(1000000),
+        get_highest_prime(100000),
+        get_highest_prime(10000)
+    ])
+    print(f"Took {round(1000*(time.time()-time0),2)} ms")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start())
+    loop.close()
